375e2d996fe0b6215d03e180b4ab71cc8d78d162
benoero99 <61589994+benoero99@users.noreply.github.com>
2021-05-05 11:17:34 +0200

    Code smellek javítása

--- BDDTest/Steps/FilterSteps.cs
+++ BDDTest/Steps/FilterSteps.cs
@@ -13,7 +13,7 @@ namespace BDDTest.Steps
     public sealed class FilterSteps
     {
         RDFGraph graph;
-        RDFResource age = new RDFResource(RDFVocabulary.RDF.BASE_URI + "age");
+        RDFResource agePred = new RDFResource(RDFVocabulary.RDF.BASE_URI + "age");
         RDFSelectQuery query = new RDFSelectQuery();
         RDFPatternGroup patternGroup;
         RDFSelectQueryResult result;
@@ -22,42 +22,42 @@ namespace BDDTest.Steps
         public void GivenCreateAGraphWithNameAndAge()
         {
             graph = new RDFGraph();
-            RDFResource person = new RDFResource(RDFVocabulary.RDF.BASE_URI + "person");
+            RDFResource personResource = new RDFResource(RDFVocabulary.RDF.BASE_URI + "person");
             RDFTypedLiteral ageLiteral = new RDFTypedLiteral("old", RDFModelEnums.RDFDatatypes.XSD_STRING);
-            graph.AddTriple(new RDFTriple(person, age, ageLiteral));
+            graph.AddTriple(new RDFTriple(personResource, agePred, ageLiteral));
             for (int i = 0; i < 10; i++)
             {
-                person = new RDFResource(RDFVocabulary.RDF.BASE_URI + "person" + i);
+                personResource = new RDFResource(RDFVocabulary.RDF.BASE_URI + "person" + i);
                 ageLiteral = new RDFTypedLiteral(new Random().Next(10, 41).ToString(), RDFModelEnums.RDFDatatypes.XSD_NONNEGATIVEINTEGER);
-                RDFTriple triple = new RDFTriple(person, age, ageLiteral);
+                RDFTriple triple = new RDFTriple(personResource, agePred, ageLiteral);
                 graph.AddTriple(triple);
             }
         }
 
         RDFVariable person;
-        RDFVariable ageLiteral;
+        RDFVariable age;
 
         [Given(@"write a select query")]
         public void GivenWriteASelectQuery()
         {
             query = new RDFSelectQuery();
             person = new RDFVariable("person");
-            ageLiteral = new RDFVariable("ageLiteral");
-            patternGroup = new RDFPatternGroup("PatternGroup").AddPattern(new RDFPattern(person, age, ageLiteral));
+            age = new RDFVariable("age");
+            patternGroup = new RDFPatternGroup("PatternGroup").AddPattern(new RDFPattern(person, age, age));
 
         }
 
         [Given(@"apply a filter so age >= ""(.*)""")]
         public void GivenApplyAFilterSoAge(int p0)
         {
-            patternGroup.AddFilter(new RDFComparisonFilter(RDFQueryEnums.RDFComparisonFlavors.GreaterOrEqualThan, ageLiteral, new RDFTypedLiteral(p0.ToString(), RDFModelEnums.RDFDatatypes.XSD_NONNEGATIVEINTEGER)));
+            patternGroup.AddFilter(new RDFComparisonFilter(RDFQueryEnums.RDFComparisonFlavors.GreaterOrEqualThan, age, new RDFTypedLiteral(p0.ToString(), RDFModelEnums.RDFDatatypes.XSD_NONNEGATIVEINTEGER)));
         }
 
         [Given(@"age must be nonnegative integer")]
         public void GivenAgeMustBeNumeric()
         {
-            patternGroup.AddFilter(new RDFDatatypeFilter(ageLiteral, RDFModelEnums.RDFDatatypes.XSD_NONNEGATIVEINTEGER));
-            query.AddPatternGroup(patternGroup).AddProjectionVariable(person).AddProjectionVariable(ageLiteral);
+            patternGroup.AddFilter(new RDFDatatypeFilter(age, RDFModelEnums.RDFDatatypes.XSD_NONNEGATIVEINTEGER));
+            query.AddPatternGroup(patternGroup).AddProjectionVariable(person).AddProjectionVariable(age);
         }
 
         [When(@"run this query on the graph")]
