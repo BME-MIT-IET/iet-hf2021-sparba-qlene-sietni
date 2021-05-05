79b22ed7acc44e44901688b5175230df9d33d212
benoero99 <61589994+benoero99@users.noreply.github.com>
2021-05-05 15:44:17 +0200

    Filter tesztek kommentezése

--- BDDTest/Steps/FilterSteps.cs
+++ BDDTest/Steps/FilterSteps.cs
@@ -19,9 +19,11 @@ namespace BDDTest.Steps
         RDFPatternGroup patternGroup;
         RDFSelectQueryResult result;
 
+        // Create RDFTriples with literals. Some of them are strings, others are non-negative integers
         [Given(@"create a graph with name and age")]
         public void GivenCreateAGraphWithNameAndAge()
-        {
+        { 
+            // RDFTriples with string literals
             graph = new RDFGraph();
             var personResource = new RDFResource(RDFVocabulary.RDF.BASE_URI + "person");
             var ageLiteral = new RDFTypedLiteral("old", RDFModelEnums.RDFDatatypes.XSD_STRING);
@@ -33,6 +35,7 @@ namespace BDDTest.Steps
             graph.AddTriple(new RDFTriple(personResource, agePred, ageLiteral));
             for (int i = 0; i < 10; i++)
             {
+                // 10 Triples but now its objects are non-negative integer literals, the number is random between 10 and 40
                 personResource = new RDFResource(RDFVocabulary.RDF.BASE_URI + "person" + i);
                 ageLiteral = new RDFTypedLiteral(new Random().Next(10, 41).ToString(), RDFModelEnums.RDFDatatypes.XSD_NONNEGATIVEINTEGER);
                 var triple = new RDFTriple(personResource, agePred, ageLiteral);
@@ -43,6 +46,7 @@ namespace BDDTest.Steps
         RDFVariable person;
         RDFVariable age;
 
+        // Select query with a pattern
         [Given(@"write a select query")]
         public void GivenWriteASelectQuery()
         {
@@ -53,12 +57,14 @@ namespace BDDTest.Steps
 
         }
 
+        // A filter on the pattern. Row only selected if age is greater or equal than 20
         [Given(@"apply a filter so age >= ""(.*)""")]
         public void GivenApplyAFilterSoAge(int p0)
         {
             patternGroup.AddFilter(new RDFComparisonFilter(RDFQueryEnums.RDFComparisonFlavors.GreaterOrEqualThan, age, new RDFTypedLiteral(p0.ToString(), RDFModelEnums.RDFDatatypes.XSD_NONNEGATIVEINTEGER)));
         }
 
+        // Another filter. Row only selected if age's tpye is non-negative integer
         [Given(@"age must be nonnegative integer")]
         public void GivenAgeMustBeNumeric()
         {
@@ -79,12 +85,15 @@ namespace BDDTest.Steps
             Assert.IsNotNull(graph);
             Assert.IsNotNull(query);
             Assert.IsNotNull(result);
+
+            // Checking results. Age column's values must be greater or equal than 20
             for (int i = 0; i < result.SelectResultsCount; i++)
             {
                 Assert.GreaterOrEqual(int.Parse(result.SelectResults.Rows[i].ItemArray[1].ToString().Split("^")[0]), p0);
             }
         }
 
+        // Filter: Row only selected if age contains "old" string
         [Given(@"apply a filter to list only ""(.*)"" people")]
         public void GivenApplyAFilterToListOnlyPeople(string p0)
         {
@@ -97,8 +106,8 @@ namespace BDDTest.Steps
         {
             Assert.IsNotNull(result);
             Assert.AreEqual(2, result.SelectResultsCount);
-            Assert.AreEqual("old", result.SelectResults.Rows[0].ItemArray[1].ToString().Split("^")[0]);
-            Assert.AreEqual("old", result.SelectResults.Rows[1].ItemArray[1].ToString().Split("^")[0]);
+            Assert.AreEqual(p0, result.SelectResults.Rows[0].ItemArray[1].ToString().Split("^")[0]);
+            Assert.AreEqual(p0, result.SelectResults.Rows[1].ItemArray[1].ToString().Split("^")[0]);
         }
 
 
